b158d67bfebc91297b392cc6a0f078ed9950abad
Mll0032 <mllawson18@gmail.com>
2024-10-17 20:32:17 -0500

    Adding annotations, Validation

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,20 +1,40 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @Size(min = 5, max = 15, message = "Username must be 5-15 characters long.")
     private String username;
+
+    @Email(message = "Invalid email")
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    private void checkPassword() {
+        if(password != null && verifyPassword != null && !password.equals(verifyPassword))
+            setVerifyPassword(null);
+    }
+
+
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
+
 
     public String getUsername() {
         return username;
@@ -38,6 +58,16 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
 
